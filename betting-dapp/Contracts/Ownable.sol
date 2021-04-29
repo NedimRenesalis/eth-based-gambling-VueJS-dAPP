fbedea3160d7f8ca9e47825cc06f458f917963e5
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-29 15:09:57 +0200

    Contract rewritten to correct error on line 43

--- betting-dapp/Contracts/Ownable.sol
+++ betting-dapp/Contracts/Ownable.sol
@@ -1,58 +1,64 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.5.1;
 
 contract Ownable {
- address owner;
- function Ownable() public {
-//Set owner to who creates the contract
- owner = msg.sender;
- }
-//Access modifier 
+  address payable owner;
 
-modifier Owned {
- require(msg.sender == owner);
- _;
- }
+  constructor () public {
+  //Set owner to who creates the contract
+  owner = msg.sender;
+  }
+
+  //Access modifier
+  modifier Owned {
+    require(msg.sender == owner);
+    _;
+  }
 }
 
 contract Mortal is Ownable {
-//Our access modifier is present, only the contract creator can      use this function
-  function kill() public Owned { 
- selfdestruct(owner);
- }
+  //Our access modifier is present, only the contract creator can      use this function
+  function kill() public Owned {
+    selfdestruct(owner);
+  }
 }
 
 contract Casino is Mortal{
- uint minBet;
- uint houseEdge; //in %
-//true+amount or false+0
+  uint minBet;
+  uint houseEdge; //in %
 
-event Won(bool _status, uint _amount);
+  //true+amount or false+0
+  event Won(bool _status, uint _amount);
 
-function Casino(uint _minBet, uint _houseEdge) payable public {
- require(_minBet > 0);
- require(_houseEdge <= 100);
- minBet = _minBet;
- houseEdge = _houseEdge;
+  constructor(uint _minBet, uint _houseEdge) payable public {
+    require(_minBet > 0);
+    require(_houseEdge <= 100);
+    minBet = _minBet;
+    houseEdge = _houseEdge;
+  }
+
+  function() external { //fallback
+    revert();
+  }
+
+  function bet(uint _number) payable public {
+    require(_number > 0 && _number <= 10, "Bet must be between 1 and 10");
+    require(msg.value >= minBet, "Bet must be bigger than minimum Bet");
+    uint winningNumber = block.number % 10 + 1;
+    if (_number == winningNumber) {
+      uint amountWon = msg.value * (100 - houseEdge)/10;
+      if(!msg.sender.send(amountWon)) revert();
+      emit Won(true, amountWon);
+    } else {
+      emit Won(false, 0);
+    }
+  }
+
+  function checkContractBalance() Owned public view returns(uint) {
+    return address(this).balance;
  }
- 
-function() public { //fallback
- revert();
+
+  function checkMinBet() Owned public view returns(uint) {
+    return minBet;
  }
 }
 
-function bet(uint _number) payable public {
- require(_number > 0 && _number <= 10);
- require(msg.value >= minBet);
- uint winningNumber = block.number % 10 + 1;
- if (_number == winningNumber) {
-   uint amountWon = msg.value * (100 — houseEdge)/10;
-   if(!msg.sender.send(amountWon)) revert();
-   emit Won(true, amountWon);
- } else {
-   emit Won(false, 0);
- }
-}
-
-function checkContractBalance() Owned public view returns(uint) {
-  return address(this).balance
- }
