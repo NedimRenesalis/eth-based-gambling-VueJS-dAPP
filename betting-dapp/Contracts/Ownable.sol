6a289830894bcf2db12b760ad004b5aa3dd62b14
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-29 14:52:54 +0200

    Formatting

--- betting-dapp/Contracts/Ownable.sol
+++ betting-dapp/Contracts/Ownable.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.10;
+
 contract Ownable {
  address owner;
  function Ownable() public {
@@ -6,22 +7,27 @@ contract Ownable {
  owner = msg.sender;
  }
 //Access modifier 
+
 modifier Owned {
  require(msg.sender == owner);
  _;
  }
 }
+
 contract Mortal is Ownable {
 //Our access modifier is present, only the contract creator can      use this function
   function kill() public Owned { 
  selfdestruct(owner);
  }
 }
+
 contract Casino is Mortal{
  uint minBet;
  uint houseEdge; //in %
 //true+amount or false+0
+
 event Won(bool _status, uint _amount);
+
 function Casino(uint _minBet, uint _houseEdge) payable public {
  require(_minBet > 0);
  require(_houseEdge <= 100);
