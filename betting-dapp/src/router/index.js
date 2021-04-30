658f7e54485e43376886b3d5a3878b954d076715
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-30 16:11:30 +0200

    update router

--- betting-dapp/src/router/index.js
+++ betting-dapp/src/router/index.js
@@ -3,11 +3,9 @@ import Router from 'vue-router'
 import CasinoDapp from '@/components/casino-dapp'
 Vue.use(Router)
 export default new Router({
- routes: [
- {
+ routes: [{
  path: '/',
  name: 'casino-dapp',
  component: CasinoDapp
- }
- ]
+ }]
 })
